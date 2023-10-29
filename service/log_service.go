ec94d0508687e9fe482af68b8283dcd10bddbd83
Mark <348393887@qq.com>
2023-10-29 18:59:11 +0800

    调整日志记录的方式

--- service/log_service.go
+++ service/log_service.go
@@ -5,24 +5,31 @@ import (
 	"log"
 	"mvc/utils"
 	"runtime"
+	"sync"
 )
 
-var Logger *log.Logger
+var (
+	Logger      *log.Logger
+	loggerMutex sync.Mutex
+)
+
+func init() {
+	utils.InitLogger()
+	Logger = utils.Logger
+}
 
 func LogInfo(v ...interface{}) {
 	message := fmt.Sprint(v...)
 
 	// 获取调用日志函数的方法名和行号
-	pc, _, line, ok := runtime.Caller(1)
+	pc, file, line, ok := runtime.Caller(1)
 	if ok {
 		funcName := runtime.FuncForPC(pc).Name()
-		message = fmt.Sprintf("[%s:%d] %s", funcName, line, message)
+		message = fmt.Sprintf("[%s:%s:%d] %s", file, funcName, line, message)
 	}
 
-	utils.InitLogger()
-
-	// 将utils.Logger赋值给全局的Logger变量
-	Logger = utils.Logger
+	loggerMutex.Lock()
+	defer loggerMutex.Unlock()
 
 	// 使用日志记录器进行日志输出
 	Logger.Println(message)
