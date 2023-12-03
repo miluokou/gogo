4bb40c9419d53ce47247bd3409dd9148111cf1de
Your Name <youremail@example.com>
2023-12-04 07:27:36 +0800

    test

--- routers/router.go
+++ routers/router.go
@@ -25,7 +25,7 @@ func SetupRouter(router *gin.Engine) {
 	//获取高德网格数据
 	router.GET("/china_girds", poi.CalculateGrid)
 
-	//基础报告页面
+	//基础报告页面1
 	router.GET("/base_report", func(c *gin.Context) {
 		// 使用HTML模板渲染网页
 		c.HTML(http.StatusOK, "basereport.html", gin.H{
